62b4e56be95b2ea770d359df434554d4616fb90a
Anton Melnychuk <78281795+anton-mel@users.noreply.github.com>
2023-09-25 23:34:02 -0400

    Update route.ts

--- app/api/getcomments/[groupId]/route.ts
+++ app/api/getcomments/[groupId]/route.ts
@@ -13,7 +13,7 @@ export async function GET (
                 "groupNetid": params.groupId
             },
             orderBy: { 
-                createdAt: 'asc',
+                createdAt: 'desc',
             },
         });
 
